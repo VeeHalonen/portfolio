6689410518cfcf123372403b24bf36b5b49c2888
VEH <veera.halonen@tuni.fi>
2020-07-24 05:39:23 +0300

    Updates

---
+++ precache-manifest.2420a0712bd1a269dd0374c6af7c5bac.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d298aeca148921bdee6c3d3b14bc94a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "093e8aa4ac967f0a8aed",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ae67dee7feb326cd74c4",
+    "url": "/portfolio/static/js/2.72b4a92d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.72b4a92d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "093e8aa4ac967f0a8aed",
+    "url": "/portfolio/static/js/main.321ceb3a.chunk.js"
+  },
+  {
+    "revision": "c67c64e704c8b24da403",
+    "url": "/portfolio/static/js/runtime-main.9a340f91.js"
+  },
+  {
+    "revision": "4f4b0eb3983f563087f108fbb7fc6ce3",
+    "url": "/portfolio/static/media/CodeDucks.4f4b0eb3.png"
+  },
+  {
+    "revision": "739eae2cee094a8a8039f98cc3cdda9a",
+    "url": "/portfolio/static/media/InteractiveStory01.739eae2c.png"
+  },
+  {
+    "revision": "099d11271505077d854af141b8840509",
+    "url": "/portfolio/static/media/InteractiveStory02.099d1127.png"
+  },
+  {
+    "revision": "26cd7aa30fd8b93b8bb6e184704b2b3c",
+    "url": "/portfolio/static/media/InteractiveStory03.26cd7aa3.png"
+  },
+  {
+    "revision": "9ea0b441d4621fe545d99135f5581467",
+    "url": "/portfolio/static/media/InteractiveStory04.9ea0b441.png"
+  },
+  {
+    "revision": "e5238b9837b4176be37a9d5ec812c4d3",
+    "url": "/portfolio/static/media/InteractiveStory05.e5238b98.png"
+  },
+  {
+    "revision": "20d3c56d6531fa18a3c5aa0c7e1859bf",
+    "url": "/portfolio/static/media/InteractiveStory06.20d3c56d.png"
+  },
+  {
+    "revision": "0298abe60789780f1e6f266986da0bcc",
+    "url": "/portfolio/static/media/bg.0298abe6.jpg"
+  },
+  {
+    "revision": "47bba2d0744d370eddd47407c9d7e09f",
+    "url": "/portfolio/static/media/fireworks.47bba2d0.jpg"
+  }
+]);
