8b69c3604f4768cb607bba9e8aebe9ea0b72c53c
VEH <veera.halonen@tuni.fi>
2021-03-02 15:17:57 +0200

    Fix links

--- src/components/content/Info.tsx
+++ src/components/content/Info.tsx
@@ -1,15 +1,10 @@
 import React from "react";
-import { Grid, Link, Typography } from "@material-ui/core";
+import { Box, Grid, Link, Typography } from "@material-ui/core";
 
 const Info = () => {
   return (
     <>
       <Grid container direction="column" spacing={3}>
-        <Grid item>
-          <Typography variant="h6" paragraph>
-            Links
-          </Typography>
-        </Grid>
         <Grid item>
           <Link
             href="https://github.com/VeeHalonen"
@@ -19,16 +14,21 @@ const Info = () => {
           >
             GitHub
           </Link>
+          <Typography>Small projects and coding practice.</Typography>
         </Grid>
         <Grid item>
           <Link
-            href="https://opintopolku.fi/koski/opinnot/f7cb0f475e0647219c6abc28f179f394"
+            href="https://opintopolku.fi/koski/opinnot/5d444f6718894be3aa4560c5249bb9f8"
             variant="h6"
             underline="none"
             paragraph
           >
             Koski
           </Link>
+          <Typography>
+            Studies and grades. Appears to be slightly glitchy when it comes to
+            showing my master's from 2016. Hard refreshing a few times may help.
+          </Typography>
         </Grid>
         <Grid item>
           <Link
